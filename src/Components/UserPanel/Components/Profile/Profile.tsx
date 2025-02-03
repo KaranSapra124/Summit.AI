556c53678a247f644b4aee148e0402eb454aab8d
KaranSapra124 <sapra7112@gmail.com>
2025-02-03 23:15:15 +0530

    Added Profile Page

--- src/Components/UserPanel/Components/Profile/Profile.tsx
+++ src/Components/UserPanel/Components/Profile/Profile.tsx
@@ -1,26 +1,34 @@
+interface planDetail {
+  type: string;
+  usage: number;
+  limit: number;
+}
 interface profileDetails {
   name: string;
   email: string;
-  plan?: object;
+  plan: planDetail;
 }
 function Profile() {
-
-    const details:profileDetails ={
-        name: "John Doe",
-        email: "john.doe@example.com",
-        plan: {
-          type: "Pro",
-          usage: 5,
-          limit: 10,
-        },
-    }
-  return <div>
-    <h1>{details?.name}</h1>
-    <h2>{details?.email}</h2>
+  const details: profileDetails = {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    plan: {
+      type: "Pro",
+      usage: 5,
+      limit: 10,
+    },
+  };
+  return (
     <div>
-        <h1>{details?.plan.type}</h1>
+      <h1>{details?.name}</h1>
+      <h2>{details?.email}</h2>
+      <div>
+        <h1>{details.plan.type}</h1>
+        <p>{details.plan.limit}</p>
+        <p>{details.plan.usage}</p>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default Profile;
