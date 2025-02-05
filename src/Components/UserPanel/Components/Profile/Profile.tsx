e040eff824ec330340860addd1d1e34445aeaaa0
karansapra124 <sapra7112@gmail.com>
2025-02-05 15:02:43 +0530

    Added Edit Profile Modal

--- src/Components/UserPanel/Components/Profile/Profile.tsx
+++ src/Components/UserPanel/Components/Profile/Profile.tsx
@@ -1,5 +1,8 @@
+import { FaEdit } from "react-icons/fa";
 import Divider from "../../../../Utils/Divider";
 import Container from "../../../Global/Container";
+import { useState } from "react";
+import Modal from "../../../Helper/Modal";
 
 interface planDetail {
   type: string;
@@ -12,7 +15,8 @@ interface profileDetails {
   plan: planDetail;
 }
 function Profile() {
-  const details: profileDetails = {
+  const [isOpen, setIsOpen] = useState<Boolean>(false);
+  const [details, setDetails] = useState<profileDetails>({
     name: "John Doe",
     email: "john.doe@example.com",
     plan: {
@@ -20,9 +24,57 @@ function Profile() {
       usage: 5,
       limit: 10,
     },
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+
+    setDetails((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
+  const profileModalData = (
+    <div className="p-6 bg-white flex flex-col relative mt-52  rounded-2xl shadow-xl w-96 mx-auto space-y-6">
+      <h1 className="text-2xl font-bold text-gray-800">Update Profile</h1>
+      <div className="space-y-4">
+        <input
+          onChange={handleChange}
+          type="text"
+          name="name"
+          value={details?.name}
+          placeholder="Enter Your Name..."
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <input
+          onChange={handleChange}
+          type="email"
+          name="email"
+          value={details?.email}
+          placeholder="Enter Your Email..."
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      <div className="flex justify-between space-x-4">
+        <button
+          onClick={() => console.log(details?.name)}
+          className="w-full px-4 py-2 font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-600 transition-all"
+        >
+          Submit
+        </button>
+        <button
+          onClick={() => setIsOpen(false)}
+          className="w-full cursor-pointer px-4 py-2 font-semibold text-gray-800 bg-gray-200 rounded-lg hover:bg-gray-300 transition-all"
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
+
   return (
     <>
+      {isOpen && <Modal data={profileModalData} />}
       <Container className="p-20 w-full">
         <div className="flex justify-between items-center shadow-sm bg-gray-100/40 rounded-lg p-2 ">
           <img
@@ -31,6 +83,10 @@ function Profile() {
             alt="No Image"
           />
           <div>
+            <FaEdit
+              onClick={() => setIsOpen(true)}
+              className="text-white text-3xl cursor-pointer hover:bg-black transition-all duration-200 ml-auto border p-1 rounded-full"
+            />
             <h1 className="text-xl font-bold text-black">
               <span className="text-gray-200 font-extrabold ">
                 Owner Name :{" "}
@@ -55,15 +111,21 @@ function Profile() {
             <table className="flex justify-between w-full rounded-sm  border border-gray-100 ">
               <td className="border  w-full border-gray-100 p-2 font-bold">
                 <tr className="text-lg  border-b-2 border-white">Plan Name</tr>
-                <td className="text-xl font-extrabold text-gray-100 py-4">{details?.plan.type}</td>
+                <td className="text-xl font-extrabold text-gray-100 py-4">
+                  {details?.plan.type}
+                </td>
               </td>
               <td className="border w-full border-gray-100 p-2 font-bold">
                 <tr className="text-lg  border-b-2 border-white">Plan Limit</tr>
-                <td className="text-xl font-extrabold text-gray-100 py-4">{details?.plan.limit}</td>
+                <td className="text-xl font-extrabold text-gray-100 py-4">
+                  {details?.plan.limit}
+                </td>
               </td>
               <td className="border w-full border-gray-100 p-2 font-bold">
                 <tr className="text-lg  border-b-2 border-white">Plan Usage</tr>
-                <td className="text-xl font-extrabold text-gray-100 py-4">{details?.plan.usage}</td>
+                <td className="text-xl font-extrabold text-gray-100 py-4">
+                  {details?.plan.usage}
+                </td>
               </td>
             </table>
             {/* <h1>{details.plan.type}</h1>
