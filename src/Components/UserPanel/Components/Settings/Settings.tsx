e54ea1a5e389ab69346f660b6ac4ef7119012332
karansapra124 <sapra7112@gmail.com>
2025-02-05 16:51:47 +0530

    Added Modal For Changing Password

--- src/Components/UserPanel/Components/Settings/Settings.tsx
+++ src/Components/UserPanel/Components/Settings/Settings.tsx
@@ -1,18 +1,51 @@
 import { useState } from "react";
 import Container from "../../../Global/Container";
+import Modal from "../../../Helper/Modal";
 
 const Settings = () => {
   const [theme, setTheme] = useState<Boolean>(false);
+  const [password, setPassword] = useState<String>("");
+  const [isPass, setIsPass] = useState<Boolean>(false);
+  const passwordModal = (
+    <div className="fixed inset-0 flex justify-center items-center bg-black/90 bg-opacity-50 z-50">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm text-center">
+        <h2 className="text-xl font-bold text-left  mb-4 text-gray-700">
+          Enter Your Email
+        </h2>
+        <input
+          type="email"
+          className="w-full p-3 mb-4 border border-gray-500 rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          placeholder="Enter Your Email..."
+        />
+        <div className="flex justify-between gap-4">
+          <button className="w-1/2 font-semibold py-2 bg-emerald-500 text-white rounded-md hover:bg-emerald-600 cursor-pointer transition-all focus:outline-none">
+            Send
+          </button>
+          <button
+            onClick={() => setIsPass(false)}
+            className="w-1/2 py-2 font-semibold cursor-pointer bg-red-500 text-white rounded-md transition-all hover:bg-red-600 focus:outline-none"
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <>
+      {isPass && <Modal data={passwordModal} />}
       <Container className="p-20 w-full">
         <h1 className="text-3xl font-bold text-gray-100">Settings</h1>
-        <div>
+        <div className="my-4">
           <div className="flex p-2 items-center justify-between w-full shadow-sm shadow-white">
             <h1 className="text-white text-xl font-semibold">
               Change Account Password
             </h1>
-            <button className="bg-red-500 text-white font-semibold p-2 rounded-sm">
+            <button
+              onClick={() => setIsPass(true)}
+              className="bg-red-500 cursor-pointer hover:bg-red-600 transition-all duration-150 text-white font-bold p-2 rounded-sm"
+            >
               Change Password
             </button>
           </div>
