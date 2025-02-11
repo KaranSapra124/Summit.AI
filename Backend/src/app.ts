20ab26ac09d2695ef4111201dd27bb99bdd8bf16
karansapra124 <sapra7112@gmail.com>
2025-02-11 11:30:56 +0530

    Configured CORS

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -11,6 +11,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 dbConfig();
+app.options("*", cors()); // Preflight requests
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
